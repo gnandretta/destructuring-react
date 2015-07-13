a33c0ab5f7be39195cc8bd98a133dc7dc874228e
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-07-12 22:04:55 -0300

    Compare ReactElements with value semantics

---
+++ value_semantics.js
@@ -0,0 +1,13 @@
+function clock(d) {
+  return React.DOM.div({id: 'clock'},
+    React.DOM.span(null, d.getHours()),
+    React.DOM.span(null, d.getMinutes()),
+    React.DOM.span(null, d.getSeconds())
+  );
+}
+
+var d = new Date(2015, 5, 30, 22, 10, 30);
+var exampleClock = clock(d);
+var anotherClock = clock(d);
+console.log(exampleClock === anotherClock); // => false
+console.log(_.isEqual(exampleClock, anotherClock)); // => true
