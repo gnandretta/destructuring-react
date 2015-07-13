b3243d8b1bb1c870ebf6a1b62a370ed85fb5f367
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-07-12 22:02:32 -0300

    Render a clock using React.render

---
+++ render.js
@@ -0,0 +1,16 @@
+function clock(d) {
+  return React.DOM.div({id: 'clock'},
+    React.DOM.span(null, d.getHours()),
+    React.DOM.span(null, d.getMinutes()),
+    React.DOM.span(null, d.getSeconds())
+  );
+}
+
+function render() {
+  var d = new Date();
+  var c = clock(d);
+  var e = document.getElementById('app');
+  React.render(c, e);
+}
+
+setInterval(render, 1000);
